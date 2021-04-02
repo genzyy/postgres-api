df992c8ae8ec504cb164dec87797721142f898de
Rishit Pandey <rishpandey8097@gmail.com>
2021-04-02 12:41:53 +0530

    documentation of postgres

--- db/queries.js
+++ db/queries.js
@@ -1,3 +1,8 @@
+/**
+ * Pool is used to connect to the postgres database with
+ * the required auth and username password.
+ */
+
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
@@ -8,6 +13,14 @@ const pool = new Pool({
   port: 5432
 });
 
+/**
+ * To write or get the data from the database, we
+ * run the appropriate query for the postgres sql database
+ * using pool.query() function and passing the query and a
+ * callback function to handle error and also to get the
+ * result.
+ */
+
 const getUsers = (req, res) => {
   pool.query('SELECT * FROM users ORDER BY id ASC', (err, result) => {
     if (err) {
@@ -17,6 +30,13 @@ const getUsers = (req, res) => {
   });
 };
 
+/**
+ * Since we may need to pass some dynamic data with the queries,
+ * we first pass the query with bash like variable indexing and
+ * then we pass the arguments in the form of array, with a callback
+ * function to get the data in return.
+ */
+
 const getUserbyId = (req, res) => {
   const id = req.params.id;
   pool.query('SELECT * FROM users WHERE id = $1', [id], (err, result) => {
@@ -24,6 +44,13 @@ const getUserbyId = (req, res) => {
     res.status(200).json(result.rows);
   });
 };
+/**
+ * When handling the POST or PUT methods
+ * instead of sending the data in json we send the data
+ * in text form because there is no point in sending a json
+ * data when it is not being displayed and since the data is only used
+ * to check the status of the method or query.
+ */
 
 const createUser = (req, res) => {
   const { name, email } = req.body;
@@ -63,6 +90,11 @@ const deleteUser = (request, response) => {
     response.status(200).send(`User deleted with ID: ${id}`);
   });
 };
+
+/**
+ * Exporting the methods so we can use them
+ * in the main file.
+ */
 
 module.exports = {
   getUsers,
